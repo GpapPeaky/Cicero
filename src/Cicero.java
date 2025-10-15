90ff79a0d4a66ddfb462e67f358ebfe419c17dff
GpapPeaky <giorgospapamat@gmail.com>
2025-10-15 20:28:34 +0300

    Base theme change

--- src/Cicero.java
+++ src/Cicero.java
@@ -5,7 +5,7 @@ public class Cicero{
         CCR_GUI gui = new CCR_GUI();
         // JFrame CCR_Window = gui.CCR_WindowCreate();
 
-        CCR_Colors.CCR_ParseColours("Octavia");
+        CCR_Colors.CCR_ParseColours("Umbra");
 
         gui.CCR_WindowCreate();
     }
